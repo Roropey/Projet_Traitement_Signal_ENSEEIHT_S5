39f6f6ce7ed1f8d74103f28010bc321808bf7c35
Roropey <romain.peyremorte@yahoo.fr>
2022-01-06 23:36:54 +0100

    oublie modif retour_bande_base.m

--- retour_bande_base.m
+++ retour_bande_base.m
@@ -4,6 +4,6 @@ if frequence==0
 else
     plage_signal=(0:T*5/(length(signal)-1):T*5);
     signal_avant_filtre=cos(2*pi*frequence*plage_signal)'.*signal;
-    signal_bande_base=filtrage_passe_bas(signal_avant_filtre,ordre_filtrage,frequence,Fe,T,false);
+    signal_bande_base=filtrage(signal_avant_filtre,ordre_filtrage,frequence,Fe,T,"bas",false);
 end
 end
