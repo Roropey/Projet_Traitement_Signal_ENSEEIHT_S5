8dce93c49462b70a451b5ff7d7695b36a1b791db
Roropey <romain.peyremorte@yahoo.fr>
2022-01-20 10:49:20 +0100

    enlève affichage s1 et s2

--- filtrage.m
+++ filtrage.m
@@ -25,34 +25,38 @@ signal_filtre=signal_decale_filtre(((ordre_de_filtrage-1)/2)+1:end);
 if Affichage
     figure('Name',strcat("Impulsion du filtre passe-",Bande," et signal filtré"));
     
+    %Question 4.1.1.1 et 4.1.2.3
     subplot(3,2,1);
     plot(plage,Impulsion,'r')
     title(strcat("Impulsion temporelle ",Bande));
     xlabel('s');
     ylabel('Amplitude');
 
+    %Question 4.1.1.2 et 4.1.2.3
     FourrierImp=abs(fftshift(fft(ifftshift(Impulsion))));
     subplot(3,2,2);
     semilogy(plage*Fe*Fe/ordre_de_filtrage,(FourrierImp),'r')
     title(strcat("Impulsion fréquentielle ",Bande));
     xlabel('Hz');
     ylabel('Module TFD');
-
+    
+    %Question 4.1.1.2 et 4.1.2.4
     subplot(3,2,[3,4]);
     module_signal=abs(fft(xcorr(signal,'unbiased')));
     plage_module=(-Fe/2:Fe/(length(module_signal)-1):Fe/2);
-    s1 = semilogy(plage_module,fftshift(module_signal),'b')
+    s1 = semilogy(plage_module,fftshift(module_signal));
     hold on;   
-    s2 = semilogy(plage*Fe*Fe/ordre_de_filtrage,(FourrierImp),'r','Linewidth',1.5)
+    s2 = semilogy(plage*Fe*Fe/ordre_de_filtrage,(FourrierImp),'r','Linewidth',1);
     hold off;
     title(strcat("Module signal et impulsion ",Bande));
     legend([s1, s2],"Module du signal","Module de l'impulsion")
     xlabel('Hz');
     ylabel('Module TFD');
-
+    
+    %Question 4.1.3
     subplot(3,2,[5 6]);    
     plage_filtre=(0:(length(signal_filtre)-1))/Fe;
-    plot(plage_filtre,signal_filtre,'b')
+    plot(plage_filtre,signal_filtre)
     title(strcat("Signal filtré par passe-",Bande));
     xlabel('Temps (s)');
     ylabel('Amplitude');
